b3a43793b6d6dc5d16f9540fffbe7d608f1acb51
Jonhatan Corona <coronajonhatan@gmail.com>
2025-06-16 23:44:47 -0500

    arreglando variables de entorno

---
+++ src/environment/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  loginEmail: process.env.LOGIN_EMAIL || '',
+  loginPassword: process.env.LOGIN_PASSWORD || '',
+};
+
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY || '',
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
+  projectId: process.env.FIREBASE_PROJECT_ID || '',
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
+  appId: process.env.FIREBASE_APP_ID || '',
+};
