63b2d9cb5afb7238beabc2f19344337a5cac831d
Jonhatan Corona <coronajonhatan@gmail.com>
2025-06-17 07:32:21 -0500

    seT

--- set-env.js
+++ set-env.js
@@ -11,7 +11,7 @@ const envConfigFile = `
 export const environment = {
   production: true,
   loginEmail: '${process.env.LOGIN_EMAIL}',
-  loginPassword: '${process.env.LOGIN_PASSWORD}'
+  loginPassword: '${process.env.LOGIN_PASSWORD}',
   firebaseConfig : {
     apiKey: '${process.env.FIREBASE_API_KEY}',
     authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
@@ -19,10 +19,8 @@ export const environment = {
     storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
     messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
     appId: '${process.env.FIREBASE_APP_ID}'
-  };
+  }
 };
-
-
+`;
 
 writeFileSync(targetPath, envConfigFile);
-console.log(`✅ Archivo environment.ts generado correctamente con exports separados.`);
